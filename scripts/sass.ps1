f8b666f22c08c66b96e49b339b4d505def82741f
Patrik Akselsson <patrik.akselsson@valtech.se>
2012-09-25 14:24:49 +0200

    simplified watch script

--- scripts/sass.ps1
+++ scripts/sass.ps1
@@ -1,5 +1,5 @@
 param (
-	$watch = $false
+	$action = "watch"
 )
 
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
@@ -8,11 +8,7 @@ $root = split-path -parent $scriptPath
 pushd $root
 try {
 	bundle install
-	if($watch){
-		bundle exec compass watch
-	} else {
-		bundle exec compass compile 
-	}
+	bundle exec compass $action
 } finally {
 	popd
 }
